af88722d981dcf64069c8a460187ecfc317e388d
Mainak <mitram@umich.edu>
2023-02-19 19:25:29 -0500

    Added comments for print.h and sent function definitions to test.cpp

--- printMessage.h
+++ printMessage.h
@@ -12,10 +12,35 @@
 #include "derivedMessage.h"
 #include <cmath>
 
+
+/**
+* Print sent baseMessage 
+* @param net_msgin message string in Network Byte Ordering
+*/
 void print_sent_baseMessage(const ustring8_t &net_msgin);
+
+/**
+* Print sent derivedMessage 
+* @param net_msgin message string in Network Byte Ordering
+*/
 void print_sent_derivedMessage(const ustring8_t &net_msgin);
+
+/**
+* Print entire last received message of baseMesaage object
+* @param msg baseMessage object
+*/
 void print_last_received_baseMesaage(const baseMessage &msg);
+
+/**
+* Print payload of last received message of derivedMessage object
+* @param msg derivedMessage object
+*/
 void print_last_received_derivedMessage_payload(const derivedMessage &msg);
+
+/**
+* Print entire last received message of derivedMessage object
+* @param msg derivedMessage object
+*/
 void print_last_received_derivedMessage(const derivedMessage &msg);
 
 #endif //_PRINTMESSAGE_
